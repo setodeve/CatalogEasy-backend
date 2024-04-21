baf0a765684f9e45c986f1757b9f841bd5d022b6
setodeve <keinick11@outlook.com>
2024-04-22 02:05:30 +0900

    feat: add catalog with user

--- app/controllers/api/catalogs_controller.rb
+++ app/controllers/api/catalogs_controller.rb
@@ -1,8 +1,11 @@
 class Api::CatalogsController < ApplicationController
   before_action :authenticate_api_user!, only: [:index,:create]
   def index
+    @catalogs = Catalog.eager_load(:user).where(users: {id: current_api_user.id})
+    render :index, formats: :json
   end
 
   def create
+    @catalog = Catalog.new(user_id: current_api_user.id)
   end
 end
