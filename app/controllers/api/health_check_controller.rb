29d3d71d4c3d4e856193fa47ca73a631a6f24238
setodeve <keinick11@outlook.com>
2024-05-01 01:58:50 +0900

    fix: fix headlth check

--- app/controllers/api/health_check_controller.rb
+++ app/controllers/api/health_check_controller.rb
@@ -1,7 +1,7 @@
 module Api
     class HealthCheckController < ApplicationController
       def index
-        render json: { message: "Success Health Check!" }, status: :ok
+        render json: { message: "Success Check!" }, status: :ok
       end
     end
 end
