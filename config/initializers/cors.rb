9d4659a637da11662d95f2bacb08ff300c1c079f
setodeve <keinick11@outlook.com>
2024-05-02 07:16:50 +0900

    fix: fix setting for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    if "production" == Rails.env
-      origins 'https://app.catlogeasy.site/'
-    else
-      origins '*'
-    end
+    origins '*'
 
     resource '*',
              headers: :any,
