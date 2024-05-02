88ba965c6f54aa4be26f44972b4dec2aa759667b
setodeve <keinick11@outlook.com>
2024-05-03 00:46:15 +0900

    fix: fix url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'localhost:80', 'localhost:3000', 'https://app.catlogeasy.site'
+    origins 'localhost:8080', 'localhost:80', 'localhost:3000', 'https://app.catalog-easy.com'
 
     resource '*',
              headers: :any,
