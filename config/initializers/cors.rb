7b3d3c23898436022b81911f4554b4156545c6f6
setodeve <keinick11@outlook.com>
2024-05-02 22:53:40 +0900

    fix: add url for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:8080', 'localhost:80', 'localhost:3000', 'https://app.catlogeasy.site'
 
     resource '*',
              headers: :any,
