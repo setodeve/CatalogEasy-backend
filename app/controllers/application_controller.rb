cf7226f1be13a60d9d61fa7db4ab6974bbf774af
setodeve <keinick11@outlook.com>
2024-05-18 01:51:29 +0900

    feat: enhance security for csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,14 @@ class ApplicationController < ActionController::API
   before_action do
     I18n.locale = :ja
   end
+  before_action :check_xhr_header
+
+ private
+  def check_xhr_header
+    return if request.xhr?
+
+    render json: { error: 'forbidden' }, status: :forbidden
+  end
 
   protected
 
